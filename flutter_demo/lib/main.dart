cafda3e5571945a70792d8ed4b2cd88fc2d13cf5
Joshua Lee <trinqk@gmail.com>
2018-05-16 18:12:47 -0700

    Done with hello world

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(
+    new Container(
+      color: Colors.blue,
+      child: new Center(
+        child: new Directionality(
+            textDirection: TextDirection.ltr,
+            child: new Text("HELLO WORLD!")
+        ),
+      ),
+    )
+  );
 }
